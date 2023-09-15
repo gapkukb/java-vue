6eda84280f213996afab5525f81b5ebfed12e22a
carlos <carlos@66hf.net>
2023-09-15 09:23:06 +0800

    udpate

--- backend/src/main/java/com/example/config/SecurityConfiguration.java
+++ backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+import com.example.util.JSONResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,43 +23,51 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.
-                authorizeHttpRequests(config -> {
-                    config.requestMatchers("/api/auth/**")
-                            .permitAll()
-                            .anyRequest()
-                            .permitAll();
-                })
-                .formLogin(config -> {
-                    config.loginProcessingUrl("/api/auth/login")
-                            .successHandler(this::onAuthenticationSuccess)
-                            .failureHandler(this::onAuthenticationFailure);
-                })
-                .logout(config->{
-                    config.logoutUrl("/logout")
-                            .logoutSuccessHandler(this::onLogoutSuccess);
-                })
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(config->config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+            authorizeHttpRequests(config -> {
+                config.requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+            })
+            .formLogin(config -> {
+                config.loginProcessingUrl("/api/auth/login")
+                    .successHandler(this::onAuthenticationSuccess)
+                    .failureHandler(this::onAuthenticationFailure)
+                ;
+            })
+            .logout(config -> {
+                config.logoutUrl("/logout")
+                    .logoutSuccessHandler(this::onLogoutSuccess)
+                ;
+            })
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
     }
 
     private void onAuthenticationSuccess(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication
     ) throws IOException, ServletException {
-        response.getWriter().write("Success");
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(JSONResponse.success().toJSONString());
     }
 
     private void onAuthenticationFailure(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            AuthenticationException exception
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException exception
     ) throws IOException, ServletException {
-        response.getWriter().write("Failure");
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(JSONResponse.fail().toJSONString());
     }
 
-    private void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    private void onLogoutSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication
+    ) throws IOException, ServletException {
 
     }
 }
--- backend/src/main/java/com/example/util/JSONResponse.java
+++ backend/src/main/java/com/example/util/JSONResponse.java
@@ -11,9 +11,29 @@ public record JSONResponse<T>(int code, T data, String message) {
 
     public static <T> JSONResponse<T> success() {
         return success(null);
+
     }
 
     public String toJSONString() throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(this);
+    }
+
+    public static <T> JSONResponse<T> fail(T data) {
+        return new JSONResponse<>(200, data, "请求失败");
+    }
+
+    public static <T> JSONResponse<T> fail(
+        int code,
+        String message
+    ) {
+        return new JSONResponse<>(code, null, message);
+    }
+
+    public static <T> JSONResponse<T> fail(String message) {
+        return fail(0,  message);
+    }
+
+    public static <T> JSONResponse<T> fail() {
+        return fail(0,  null);
     }
 }
